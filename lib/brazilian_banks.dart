051166a8ff22cea011a51b78f2248f7cc1ad1150
Gustavo Granado Saccardo <gstvsaccardo.gg@gmail.com>
2021-12-07 14:14:31 -0300

    feat(export): new features

--- lib/brazilian_banks.dart
+++ lib/brazilian_banks.dart
@@ -3,3 +3,7 @@ library brazilian_banks;
 export 'src/models/bank_account_model.dart';
 export 'src/models/bank_account_validation_model.dart';
 export 'src/models/brasil_api_banks_model.dart';
+export 'src/widgets/account_input_formatter.dart';
+export 'src/widgets/bank_account_text_form_field.dart';
+export 'src/widgets/bank_branch_text_form_field.dart';
+export 'src/widgets/branch_input_formatter.dart';
