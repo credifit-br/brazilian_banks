94d716ba1bc4729c07c6399c35abcc3dd3338146
Gustavo Granado Saccardo <gstvsaccardo.gg@gmail.com>
2021-12-07 14:07:16 -0300

    feat(validation): Bradesco branch validator

--- lib/src/controllers/branch_validation/bradesco_branch_validator.dart
+++ lib/src/controllers/branch_validation/bradesco_branch_validator.dart
@@ -6,12 +6,46 @@ extension on String {
       [substring(0, length - _length), substring(length - _length)];
 }
 
-const _accountLen = 7;
+const _branchLen = 4;
 
 /// Bradesco - 237
 /// @param accountNumberWithDigit: can be in format "#-0" or "#0"
 BankBranchValidation bradescoBranchValidator(
   BankBranchModel bankBranchModel,
 ) {
-  return BankBranchValidation();
+  final _bankBranchValidation = BankBranchValidation();
+
+  final _branch = bankBranchModel.branchNumber
+      .replaceAll("-", "")
+      .replaceAll(".", "")
+      .splitByLength(1);
+  final _branchNumber = _branch[0].padLeft(_branchLen, '0');
+  final _numbers = _branchNumber.split("");
+
+  var sumSequence = 0;
+  var sequence = 0;
+
+  for (var i = 0; i < _numbers.length; i++) {
+    sequence = 5 - i;
+    sumSequence += int.parse(_numbers[i]) * sequence;
+  }
+
+  final digit = _module(sumSequence);
+
+  _bankBranchValidation.isValid = digit == _branch[1];
+  _bankBranchValidation.digit = digit;
+  _bankBranchValidation.branch = _branchNumber;
+
+  return _bankBranchValidation;
+}
+
+String _module(int sumSequence) {
+  final result = 11 - (sumSequence % 11);
+  if (result == 10) {
+    return "P";
+  }
+  if (result == 11) {
+    return "0";
+  }
+  return result.toString();
 }
